1b50ef133ad2fb2d21ab842f11693c09e9af5613
Mario Nataren <mario.nataren@gmail.com>
2020-05-11 01:03:13 -0500

    De hijo a padre

--- PADRE-HIJO/componentes/hijo.js
+++ PADRE-HIJO/componentes/hijo.js
@@ -3,7 +3,16 @@ Vue.component ('hijo',{
     `
     <div class="py-5 bg-success">
         <h4 >Componente Hijo: {{numero}}</h4>
+        <h4> Nombre: {{nombre}}</h4>
     </div>
     `,
-    props:['numero']
+    props:['numero'],
+    data(){
+        return{
+            nombre: 'MArio'
+        }
+    },
+    mounted(){
+        this.$emit('nombreHijo',this.nombre)
+    }
 })
