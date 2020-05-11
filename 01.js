1fa8bc82a7f2978e93f27b2205d43dbeb47ed843
Mario Nataren <mario.nataren@gmail.com>
2020-05-10 19:39:40 -0500

    Se agrego funcion de aumentar mas uno y cambiar la catidad de frutas, asi como totalizar todas las frutas

--- 01.js
+++ 01.js
@@ -7,13 +7,24 @@ const app = new Vue({
             {nombre:'Manzana', cantidad:0},
             {nombre:'Platano', cantidad:12},
         ],
-        nuevafruta: ''
+        nuevafruta: '',
+        total: 0
     },
     methods: {
         agregarFruta (){
             this.frutas.push({
                 nombre: this.nuevafruta, cantidad: 0
-            })
+            });
+            this.nuevafruta = '';
+        }
+    },
+    computed: {
+        sumarfrutas (){
+            this.total= 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
         }
     }
 })
