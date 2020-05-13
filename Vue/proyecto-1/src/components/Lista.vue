8083d25ea20965babc0283b4965f01387f5950f7
Mario Nataren <mario.nataren@gmail.com>
2020-05-12 23:51:31 -0500

    INICIAMOS CON VUE CLI, FRUTAS

---
+++ Vue/proyecto-1/src/components/Lista.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                Manzana
+                <span class="badge badge-primary badge-pill">14</span>
+            </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+                Pera
+                <span class="badge badge-primary badge-pill">2</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+                Sandia
+                <span class="badge badge-primary badge-pill">1</span>
+                </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Lista'
+}
+</script>
