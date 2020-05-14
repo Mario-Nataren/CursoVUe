c83aac132a5f5232484e5fab267372ee3e6a7595
Mario Nataren <mario.nataren@gmail.com>
2020-05-14 00:41:37 -0500

    Ordenar array, hay utilzar el state para que funcione

--- Vue/proyecto-1/src/components/Lista.vue
+++ Vue/proyecto-1/src/components/Lista.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul class="list-group">
-            <li v-for="(item, index) of frutas" :key="item.id" @click="aumentar(index)"
+            <li v-for="(item, index) of arrayordenado" :key="item.id" @click="aumentar(index)"
             class="list-group-item d-flex justify-content-between align-items-center">
                 {{item.nombre}}
                 <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
@@ -16,8 +16,17 @@ import{mapState, mapMutations} from 'vuex';
 export default {
     name: 'Lista',
     computed:{
-        ...mapState(['frutas']) //lleva un array y adentro lleva el nombre del state que usaremos
-
+        ...mapState(['frutas']), //lleva un array y adentro lleva el nombre del state que usaremos
+        
+//         arrayOrdenado(state){
+// var orden = (a, b) => b.cantidad - a.cantidad;
+// return state.frutas.sort(orden);
+// }
+        
+        
+        arrayordenado(state){
+            return state.frutas.sort( (a , b) => b.cantidad - a.cantidad); //sort es  una funcion de punto de comparacion
+        }
     },
     methods:{
         ...mapMutations (['aumentar','reset']),
