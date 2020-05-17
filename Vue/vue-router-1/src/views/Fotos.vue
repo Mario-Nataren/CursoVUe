6a9d9ad6b6d3e27d1fdc689cb0145f730378ee7f
Mario Nataren <mario.nataren@gmail.com>
2020-05-17 00:04:09 -0500

    Termine la seccion de Vue router

--- Vue/vue-router-1/src/views/Fotos.vue
+++ Vue/vue-router-1/src/views/Fotos.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
         <h1>Galeria de foto</h1>
+        
+
+        <router-link :to="{name: 'Fotos', params: {id : item}}" 
+        v-for="(item,index) of fotosArreglo" :key="index">
+            <button>Foto {{item}}</button>
+        </router-link>
+        <hr>
+        <div>
+              <button @click="Home">Home</button>
+              <button @click="Anterior">Anterior</button>
+              <button @click="Siguiente">Siguiente</button>
+        </div>
+
+
         <Foto/>
+
     </div>
 </template>
 <script>
@@ -9,6 +24,22 @@ import Foto from "@/components/Fotografia.vue"
 export default {
     components: {
         Foto
+    },
+    data(){
+        return{
+            fotosArreglo:[1,2,3,4]
+        }
+    },
+    methods:{
+        Home(){
+            this.$router.push('/')
+        },
+        Anterior(){
+            this.$router.go(-1)
+        },
+        Siguiente(){
+            this.$router.go(1)
+        }
     }
 }
 </script>
